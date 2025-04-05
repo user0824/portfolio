dc0b73639156a351d70973c7270ff55d0f430fc3
Amit <149993488+user0824@users.noreply.github.com>
2025-04-04 21:21:27 -0400

    BUILD: Inititialized front end.

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import TestComponent from "./components/TestComponent";
+
+const App = () => {
+  return (
+    <>
+      <TestComponent />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/TestComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestComponent: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-teal-500 text-9xl font-extrabold text-center">hello</p>
+    </div>
+  );
+};
+
+export default TestComponent;
