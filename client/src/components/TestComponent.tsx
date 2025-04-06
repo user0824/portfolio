6ff884148dc0aae89bec765132aaad4f43e51d9e
Amit <149993488+user0824@users.noreply.github.com>
2025-04-05 20:02:08 -0400

    test: testing branch protection rules and github actions

--- client/src/components/TestComponent.tsx
+++ client/src/components/TestComponent.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const TestComponent: React.FC = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <p className="text-center text-9xl font-extrabold text-teal-500">hello</p>
+      <p className="text- text-center text-9xl font-extrabold text-pink-300">
+        hello
+      </p>
     </div>
   );
 };
